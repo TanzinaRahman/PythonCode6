8682fd10dc2b72ff0756c9490a163c19973e68c4
Tanzina Rahman Mona <77913975+TanzinaRahman@users.noreply.github.com>
2021-07-08 00:13:01 +0600

    Create PythonProgram6.py

---
+++ PythonProgram6.py
@@ -0,0 +1,8 @@
+# Type Casting
+
+num1 = int(input("Enter your first number  : "))
+num2 = int(input("Enter your second number : "))
+Result = num1 + num2
+print(Result)
+Result = num1 - num2
+print("The Result is num1 - num2 : " , Result)
